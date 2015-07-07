145bce1b5c239a74e524879e73c2f8352cc6f7f0
Calvin Wang <calvinkw1@gmail.com>
2015-07-06 22:44:28 -0700

    generate source function basics working, still need to add specific cases depending on input type selection

--- app.js
+++ app.js
@@ -36,20 +36,21 @@ $(document).ready(function() {
 
   });
 
+  var labelVals = [],
+      typeVals = [],
+      requiredVals = [];
   $("#generate").click(function(e) {
     e.preventDefault();
-    counter = 1; // reset counter on click of generate button
+    // counter = 1; // reset counter in case generate button is clicked again
+    $("tbody").empty(); // clear table's tbody in case generate button is clicked again
     generateTableData();
-    // generateSource();
+    generateSource();
   });  
 
 
 
   function generateTableData() {
-    var labelVals = [],
-        typeVals = [],
-        requiredVals = [];
-    for (var i = 0; i < labelIDs.length; i++) {
+    for (var i = 0; i < labelIDs.length; i++) { // labelIDs, typeIDs, and requiredIDs arrays will always be same length
       labelVals.push($("#label-" + i).val());
       typeVals.push($("#type-" + i).val());
       requiredVals.push($("#required-" + i).val());
@@ -57,7 +58,7 @@ $(document).ready(function() {
     console.log(labelVals);
     console.log(typeVals);
     console.log(requiredVals);
-    for (var j = 0; j < labelVals.length; j++) {
+    for (var j = 0; j < labelVals.length; j++) { // labelVals, typeVals, and requiredVals arrays will always be same length
       var html = "<tr>";
       html += "<td>" + labelVals[j] + "</td>";
       html += "<td>" + typeVals[j] + "</td>";
@@ -68,9 +69,17 @@ $(document).ready(function() {
   }
 
   function generateSource() {
-
+    var html;
+    for (var i = 0; i < labelVals.length; i++) {
+      $("pre").append("    &lt;label for='" + labelVals[i].toLowerCase() + "'>" + labelVals[i] + ": &lt;label><br>");
+      if (requiredVals[i] === "yes") {
+        $("pre").append("    &lt;input type='" + typeVals[i] + "' id='" + labelVals[i].toLowerCase() + "' required><br>");
+      } else {
+        $("pre").append("    &lt;input type='" + typeVals[i] + "'><br>");
+      }
+    }
+    $("pre").append("    &lt;input type='submit'><br>");
+    $("pre").append("  &lt;/form>");
   }
-
-
 
 });
