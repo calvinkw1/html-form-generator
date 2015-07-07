deef00c5f2314db3850aa72bb5254b1d52677154
Calvin Wang <calvinkw1@gmail.com>
2015-07-06 21:15:25 -0700

    generate table data function working and now appends to the table div

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
 
+  var labelIDs = [0],
+      typeIDs = [0],
+      requiredIDs = [0];
+
   var counter = 1;
   $("#new-field").click(function() {
     var html = '';
-    html += "<li id=" + counter + ">";
+    labelIDs.push($(counter));
+    html += "<li>";
     html += "<label for='label'> Input Label: </label>";
-    html += "<input type='text' required autofocus>";
+    html += "<input type='text' class='labels' id='label-" + counter + "' required autofocus>";
     html += "<label for='type'> Type: </label>";
-    html += "<select name='type' id='type'>";
+    html += "<select name='type' class='types' id='type-" + counter + "'>";
     html += "<option value='text' selected>Text</option>";
     html += "<option value='multiline-text'>Multiline-Text</option>";
     html += "<option value='date'>Date</option>";
@@ -16,7 +21,7 @@ $(document).ready(function() {
     html += "<option value='username'>Username</option>";
     html += "</select>";
     html += "<label for='required'> Required Field? </label>";
-    html += "<select name='required' id='required'>";
+    html += "<select name='required' class='required' id='required-" + counter + "'>";
     html += "<option value='yes'>Yes</option>";
     html += "<option value='no'>No</option>";
     html += "</select>";
@@ -27,22 +32,43 @@ $(document).ready(function() {
     // $('.remove').click(function() {
     //   console.log(this);
     // });
+
+
   });
 
-  
-
-  $("form").submit(function(e) {
+  $("#generate").click(function(e) {
     e.preventDefault();
-    // generateTableData();
+    counter = 1; // reset counter on click of generate button
+    generateTableData();
     // generateSource();
-  });
+  });  
+
+
 
   function generateTableData() {
-    $("");
+    var labelVals = [],
+        typeVals = [],
+        requiredVals = [];
+    for (var i = 0; i < labelIDs.length; i++) {
+      labelVals.push($("#label-" + i).val());
+      typeVals.push($("#type-" + i).val());
+      requiredVals.push($("#required-" + i).val());
+    }
+    console.log(labelVals);
+    console.log(typeVals);
+    console.log(requiredVals);
+    for (var j = 0; j < labelVals.length; j++) {
+      var html = "<tr>";
+      html += "<td>" + labelVals[j] + "</td>";
+      html += "<td>" + typeVals[j] + "</td>";
+      html += "<td>" + requiredVals[j] + "</td>";
+      html += "</tr>";
+      $("tbody").append(html);
+    }
   }
 
   function generateSource() {
-    
+
   }
 
 
