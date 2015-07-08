8a92007a5e00afe6dcaddcfd186039fc725937fa
Calvin Wang <calvinkw1@gmail.com>
2015-07-08 12:00:55 -0700

    form generation function refactored to separate html from js

--- app.js
+++ app.js
@@ -2,57 +2,29 @@ $(document).ready(function() {
 
   var labelIDs = [0],
       typeIDs = [0],
-      requiredIDs = [0];
-
-  var counter = 1;
-  // $("#new-field").click(function() {
-  //   var html = '';
-  //   labelIDs.push(counter);
-  //   html += "<li>";
-  //   html += "<label for='label'> Input Label: </label>";
-  //   html += "<input type='text' class='labels' id='label-" + counter + "' required autofocus>";
-  //   html += "<label for='type'> Type: </label>";
-  //   html += "<select name='type' class='types' id='type-" + counter + "'>";
-  //   html += "<option value='text' selected>Text</option>";
-  //   html += "<option value='multiline-text'>Multiline-Text</option>";
-  //   html += "<option value='date'>Date</option>";
-  //   html += "<option value='email'>Email</option>";
-  //   html += "<option value='password'>Password</option>";
-  //   html += "<option value='username'>Username</option>";
-  //   html += "</select>";
-  //   html += "<label for='required'> Required Field? </label>";
-  //   html += "<select name='required' class='required' id='required-" + counter + "'>";
-  //   html += "<option value='yes'>Yes</option>";
-  //   html += "<option value='no'>No</option>";
-  //   html += "</select>";
-  //   // html += "<input type='button' class='remove' value='Remove'";
-  //   html += "</li>";
-  //   $("ul").append(html);
-  //   counter += 1;
-  //   // $('.remove').click(function() {
-  //   //   console.log(this);
-  //   // });
-  // });
-
-// ^^ above function is the same as below function, but refactored based on Martin's suggestion
+      requiredIDs = [0],
+      counter = 1,
+      labelVals = [],
+      typeVals = [],
+      requiredVals = [];
 
 // clones the original input row and changes the id #s for each input before appending to the list element
   $("#new-field").click(function() {
     var newLabelID = "label-" + counter,
         newTypeID = "type-" + counter,
         newRequiredID = "required-" + counter,
-        $clonedElement = $("#orig-input").clone().removeAttr("id").addClass("cloned");
-        $clonedElement.find("#label-0").attr("id", newLabelID);
-        $clonedElement.find("#type-0").attr("id", newTypeID);
-        $clonedElement.find("#required-0").attr("id", newRequiredID);
-        $clonedElement.appendTo($("ul"));
-    counter += 1;
+        $clonedField = $("#orig-input").clone().removeAttr("id").addClass("cloned");
+        $clonedField.find("#label-0").attr("id", newLabelID);
+        $clonedField.find("#type-0").attr("id", newTypeID);
+        $clonedField.find("#required-0").attr("id", newRequiredID);
+        $clonedField.appendTo($("ul"));
+    labelIDs.push(newLabelID);
+    typeIDs.push(newTypeID);
+    requiredIDs.push(newRequiredID);
+    counter++;
   });
 
-  var labelVals = [],
-      typeVals = [],
-      requiredVals = [];
-  $("#generate").click(function(e) {
+  $("form").submit(function(e) {
     e.preventDefault();
     // counter = 1; // reset counter in case generate button is clicked again
     $("tbody").empty(); // clear table's tbody in case generate button is clicked again
@@ -64,7 +36,6 @@ $(document).ready(function() {
   $("#view-source").click(function() {
     $("#source-block").slideDown();
   });
-
 
   function generateTableData() {
     for (var i = 0; i < labelIDs.length; i++) { // labelIDs, typeIDs, and requiredIDs arrays will always be same length
@@ -84,6 +55,62 @@ $(document).ready(function() {
   }
 
   function generateForm() {
+    firstInputSetAttrs();
+    clonedInputSetAttrs();
+    $("#generated-form").show('slide');
+  }
+
+  function firstInputSetAttrs() {
+    var $firstInput = $("#first-input");
+    $firstInput.find('label').attr('for', labelVals[0]);
+    $firstInput.find('label').text(labelVals[0]);
+    if (typeVals[0] === "multiline-text") {
+      $firstInput.find('input').replaceWith("<textarea name='" + labelVals[0].toLowerCase() + "' id='" + labelVals[0].toLowerCase() + "' cols='30' rows='10'>");
+      if (requiredVals[0] === "yes") {
+        $firstInput.find("textarea").prop("required", true);
+      }
+    } else {
+      if (requiredVals[0] === "yes") {
+        $firstInput.find('input').prop({
+          type: typeVals[0],
+          id: labelVals[0],
+          required: true
+        });
+      }
+      else {
+        $firstInput.find('input').attr({
+          type: typeVals[0],
+          id: labelVals[0]
+        });
+      }
+    }
+    $firstInput.appendTo($("#generated-form"));
+  }
+
+  function clonedInputSetAttrs() {
+    for (var i = 1; i < labelVals.length; i++) {
+      var $clonedInput = $("#first-input").clone().removeAttr("id").addClass("cloned-input");
+      $clonedInput.find("label").attr("for", labelVals[i]);
+      $clonedInput.find("label").text(labelVals[i]);
+      if (typeVals[i] === "multiline-text") {
+        $clonedInput.find('input').replaceWith("<textarea name='" + labelVals[i].toLowerCase() + "' id='" + labelVals[i].toLowerCase() + "' cols='30' rows='10'>");
+        if (requiredVals[i] === "yes") {
+          $clonedInput.find("textarea").prop("required", true);
+        }
+      } else {
+        $clonedInput.find('input').attr({
+          type: typeVals[i],
+          id: labelVals[i]
+        });
+        if (requiredVals[i] === "yes") {
+          $clonedInput.find("input").prop("required", true);
+        }
+      }
+    $clonedInput.appendTo($("#generated-form"));
+    }
+  }
+
+  function genForm() {
     var html = "";
     for (var i = 0; i < labelVals.length; i++) {
       html += "<form action='#'>";
@@ -113,7 +140,6 @@ $(document).ready(function() {
       $("pre").append("    &lt;label for='" + labelVals[i].toLowerCase() + "'>" + labelVals[i] + ": &lt;label><br>");
       if (typeVals[i] === "multiline-text") {
         var html = "    &lt;textarea name='" + labelVals[i].toLowerCase() + "' id='" + labelVals[i].toLowerCase() + "' cols='30' rows='10'";
-        console.log(html);
         if (requiredVals[i] === "yes") {
           $("pre").append(html + " required>&lt;/textarea><br>");
         } else {
